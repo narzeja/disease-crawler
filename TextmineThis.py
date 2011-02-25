f09e28a18a9c1eb77d9503b3b9fe9f855d9b3a3d
Brian Mathiasen <bmathiasen@gmail.com>
2011-02-25 15:01:24 +0100

    jizzle

--- TextmineThis.py
+++ TextmineThis.py
@@ -207,7 +207,7 @@ class Textminer:
         # Sanitize query terms
         query = sanitizer.sub(' ',query)
         
-        if isinstance(query,list):saniti
+        if isinstance(query,list):
             searchTerms = [s.strip().lower() for s in query if s!='']
         elif isinstance(query,str):
             searchTerms = [s.strip().lower() for s in query.split(' ') if s!='']
--- everything.py
+++ everything.py
@@ -3,16 +3,16 @@ import pod
 import GCrawler as GC
 import FeatureExtractor as FE
 
-def everything(num, disease):
+def everything(num, disease, extra="", strict=True):
     g = GC.GCrawler()
     data = pod.parseOrphaDesc()
     fe = FE.FeatureExtractor()
 
     newdata = g._convert_pod(data)
-    lots = g.crawlGoogle(newdata[num:num+1], "characterized by")
+    lots = g.crawlGoogle(newdata[num:num+1], extra)
     crack = ' '.join([' '.join(s) for s in lots[disease]])
 
-    raw, cands = fe.feature_extractor(crack)
+    raw, cands = fe.feature_extractor(crack, strict)
     return lots, crack, raw, cands
 #    hits, miss = fe.validate_features(cands)
 #    return hits, miss
