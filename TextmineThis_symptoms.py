fe1a9640deb1491be1442aa9570643069b7c3376
henne (live4adrenalin <henne (live4adrenalin@gmail.com)>
2011-03-18 14:18:53 +0100

    same...

--- TextmineThis_symptoms.py
+++ TextmineThis_symptoms.py
@@ -95,7 +95,7 @@ class Textminer:
 #        print "Converting to lil matrix format..."
 #        tfidf = termDoc.tolil()
 #        print "Converting to csr and counting terms/doc occurences."
-        term_counts = float(sum(termDoc.T>0).tolist()[0])
+        term_counts = sum(termDoc.T>0).tolist()[0]
         
         print "Running TF-IDF..."
         for row in range(0,termDoc.shape[0]):
@@ -109,7 +109,7 @@ class Textminer:
                 
                 print col,row
                 try:
-                    idf = math.log(termDoc.shape[0] / term_counts[col])
+                    idf = math.log(termDoc.shape[0] / float(term_counts[col]))
                 except:
                     print 
                     raise ZeroDivisionError
