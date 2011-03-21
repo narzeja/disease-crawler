5c4a9ba0ebc4f7645214a4aeb21aa24f03b970a8
henne (live4adrenalin <henne (live4adrenalin@gmail.com)>
2011-03-21 12:24:17 +0100

    playing with icd10

--- TextmineThis_symptoms.py
+++ TextmineThis_symptoms.py
@@ -168,8 +168,8 @@ class Textminer:
             rev_doc_hash = dict(zip(doc_hash.values(),doc_hash.keys()))
             print len(docs)
             for doc in docs:
-                print doc
-                score = termDoc[doc,n]
+                score = sum(termDoc[doc,n])
+                print score
                 
                 doc_id = rev_doc_hash[doc] # extract the original orpha-nums 
                 
