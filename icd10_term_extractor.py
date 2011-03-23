a5e9bfb215dbafa468221e19929d539aa6465b5e
henne (live4adrenalin <henne (live4adrenalin@gmail.com)>
2011-03-23 11:44:21 +0100

    wokring on nlp-icd10 combination

--- icd10_term_extractor.py
+++ icd10_term_extractor.py
@@ -1,15 +1,28 @@
 import db as DB
+import itertools
 #import TextmineThis as TT
 
 
 def run(TFIDF,TermDoc,t_hash,d_hash,code):
     
     db = DB.db()
-    codes = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","V","Z"]
     
     #patres INT, code TEXT, category TEXT, keywords TEXT,
     icd10 = db.c.execute("select patres,code from icd_10").fetchall()
     relevant_patreses = [x[0] for x in icd10 if code in x[1]]
+    
+    # Get the symptoms extracted for the icd10 group
+    symptom_list=[]
+    for patres in relevant_patreses:
+        symptoms = db.c.execute("select N.symptom \
+                                 from nlp_nonweighted N \
+                                 where N.patres=?"[patres]).fetchall()
+        symptoms = [x[0] for x in symptoms if x]
+        symptom_list.extend(symptoms)
+    symptom_list = set(symptom_list)
+    # try splitting it...hacked version!
+    symptom_list = list(itertools.chain(*[x.split() for x in symptom_list]))
+    
     
     # Get the diseases belonging to the icd 10 category
     rows = [d_hash[x] for x in relevant_patreses]
@@ -24,7 +37,10 @@ def run(TFIDF,TermDoc,t_hash,d_hash,code):
     rev_term_hash = dict(zip(t_hash.values(),t_hash.keys()))
     sorted_tfidf_terms = [rev_term_hash[x] for x in scores_tfidf]
     
-    return sorted_tfidf_terms
+    # remove non-symptom candidates
+    sorted_tfidf_terms_cleaned = [x for x in sorted_tfidf_terms if x in symptom_list]
+    
+    return sorted_tfidf_terms, sorted_tfidf_terms_cleaned
     
 #    submatrix_termDoc = TermDoc[rows,:]
     
