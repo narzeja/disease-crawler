8e18d4165eddb491129f842c1ec1bd37522f992f
henne (live4adrenalin <henne (live4adrenalin@gmail.com)>
2011-03-23 16:57:15 +0100

    wokring on nlp-icd10 combination

--- icd10_term_extractor.py
+++ icd10_term_extractor.py
@@ -30,10 +30,10 @@ def run(TFIDF,TermDoc,t_hash,d_hash):
         # Get the diseases belonging to the icd 10 category
         rows=[]
         for patres in relevant_patreses:
+            if patres==130: print code
             try:
                 rows.append(d_hash[patres])
             except: 
-                if patres==130: print "Damn!"
                 missing_patreses.append(patres)
                 continue
         
