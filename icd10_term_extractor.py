bf79def52bedfd7f660ef2382fc4ba85af5e4491
henne (live4adrenalin <henne (live4adrenalin@gmail.com)>
2011-03-23 15:00:14 +0100

    wokring on nlp-icd10 combination

--- icd10_term_extractor.py
+++ icd10_term_extractor.py
@@ -9,53 +9,65 @@ def run(TFIDF,TermDoc,t_hash,d_hash,code):
     db = DB.db()
     tts = TTS.Textminer()
     
-    #patres INT, code TEXT, category TEXT, keywords TEXT,
-    icd10 = db.c.execute("select patres,code from icd_10").fetchall()
-    print len(icd10)
-    relevant_patreses = [x[0] for x in icd10 if code in x[1]]
+    codes=["A","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","V","Z"]
     
-    # Get the symptoms extracted for the icd10 group
+    icd10 = db.c.execute("select patres,code from icd_10").fetchall()
+    
+    # Get all the symptoms extracted by NLP
     symptoms = db.c.execute("select N.freq from nlp_nonweighted N").fetchall()
     symptoms = [x[0] for x in symptoms if x]
     symptom_list = list(itertools.chain(*[str(x).split() for x in symptoms]))
     symptom_list = [tts.stem(x) for x in symptom_list]
     symptom_list = set(symptom_list)
     
-#    symptom_list=[]
-#    for patres in relevant_patreses:
-#        symptoms = db.c.execute("select N.freq \
-#                                 from nlp_nonweighted N \
-#                                 where N.patres=?",[patres]).fetchall()
-#        symptoms = [x[0] for x in symptoms if x]
-#        symptom_list.extend(symptoms)
-#    # try splitting it...hacked version!
-#    symptom_list = list(itertools.chain(*[x.split() for x in symptom_list]))
-#    symptom_list = set(symptom_list)
+    icd_featurevectors = {}
+    missing_patreses=[]
+    for code in codes:
+        # Get diseases belonging to the icd 10 group
+        relevant_patreses = [x[0] for x in icd10 if code in x[1]]
+        
+        # Get the diseases belonging to the icd 10 category
+        rows=[]
+        for patres in relevant_patreses:
+            try:
+                rows.append(d_hash[patres])
+            except: 
+                missing_patreses.append(patres)
+                continue
+        
+        # Get the indices (aka. the term hashes) sorted by summed-tfidf score
+        submatrix_tfidf = TFIDF[rows,:]
+        scores = sum(submatrix_tfidf[:]).tolist()[0]
+        scores_tfidf = range(len(scores))
+        scores_tfidf.sort(lambda x,y: cmp(scores[x],scores[y]),reverse=True)
+        
+        # Reverse term hash (terms can be looked up by hash)
+        rev_term_hash = dict(zip(t_hash.values(),t_hash.keys()))
+        
+        sorted_tfidf_terms = [(rev_term_hash[x], for x in scores_tfidf]
+        
+        # remove non-symptom candidates
+        sorted_tfidf_terms_cleaned = [x for x in sorted_tfidf_terms if x in symptom_list]
+        
+        icd_featurevectors[code] = sorted_tfidf_terms_cleaned
     
+    return icd_featurevectors
+
+def locate_term_category(term,icd_featurevectors):
     
-    # Get the diseases belonging to the icd 10 category
-    rows=[]
-    for patres in relevant_patreses:
-        try:
-            rows.append(d_hash[patres])
-        except: continue
-#    rows = [d_hash[x] for x in relevant_patreses]
+    ranked_groups=[]
+    for code,feature_vec in icd_featurevectors.items():
+        if term in feature_vec:
+            ranked_groups.append((feature_vec.index(term),code))
+     
+     return ranked_groups
+
+def locate_entire_query(query,icd_featurevectors):
     
-    submatrix_tfidf = TFIDF[rows,:]
+    ranked_terms = {}
+    for term in query.split():
+        if term:
+            ranked_groups = locate_term_category(term,icd_featurevectors)
+            ranked_terms[term] = sorted(ranked_groups,reverse=True)
     
-    # Get the indices (aka. the term hashes) sorted by summed-tfidf score
-    scores = sum(submatrix_tfidf[:]).tolist()[0]
-    scores_tfidf = range(len(scores))
-    scores_tfidf.sort(lambda x,y: cmp(scores[x],scores[y]))
-    
-    rev_term_hash = dict(zip(t_hash.values(),t_hash.keys()))
-    sorted_tfidf_terms = [rev_term_hash[x] for x in scores_tfidf]
-    
-    # remove non-symptom candidates
-    sorted_tfidf_terms_cleaned = [x for x in sorted_tfidf_terms if x in symptom_list]
-    
-    return sorted_tfidf_terms, sorted_tfidf_terms_cleaned,symptom_list
-    
-#    submatrix_termDoc = TermDoc[rows,:]
-    
-#    return scores_tfidf
+    return ranked_terms
