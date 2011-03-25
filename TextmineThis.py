1e788341e8aacbf5ee0a74d4543fa321e9afc988
henne (live4adrenalin <henne (live4adrenalin@gmail.com)>
2011-03-25 16:05:10 +0100

    working on some gram-method

--- TextmineThis.py
+++ TextmineThis.py
@@ -240,7 +240,7 @@ class Textminer:
                 
                 docs.append(list(set(termDoc[:,n].nonzero()[0].tolist()[0])))
             
-            print docs
+            docs = docs[0]
             fq = nltk.FreqDist(docs)
             docs = [x for x in docs if fq[x]==len(terms)]
             
