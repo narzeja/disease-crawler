0aafa1c94a9ca5cc01335381734e15a9cc1bd26a
henne (live4adrenalin <henne (live4adrenalin@gmail.com)>
2011-03-26 14:50:16 +0100

    class'ifying the test class

--- icd10_term_extractor.py
+++ icd10_term_extractor.py
@@ -86,7 +86,7 @@ class ICD10tester(object):
         
         """
         # Number of candidate categories to be included from each term
-        numcat = 3
+        numcat = 4
         
         # Calculate the sorted list of icd 10 categories of each term
         ranked_terms = {}
