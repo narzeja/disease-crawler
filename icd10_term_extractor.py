712365d1b5109d635972f1a9a107d23ba1a88f25
henne (live4adrenalin <henne (live4adrenalin@gmail.com)>
2011-03-26 15:32:59 +0100

    working on some gram-method

--- icd10_term_extractor.py
+++ icd10_term_extractor.py
@@ -87,7 +87,7 @@ class ICD10tester(object):
         
         """
         # Number of candidate categories to be included from each term
-#        numcat = 4
+        numcat = 3
         
         # Calculate the sorted list of icd 10 categories of each term
         ranked_terms = {}
@@ -100,15 +100,15 @@ class ICD10tester(object):
                 ranked_terms[term] = sorted(ranked_groups)
         
         # Get the icd 10 categories defining the reduced search-space
-        numcat = 1
+#        numcat = 1
         potentials = []
-        while len(potentials)<=3:
-            numcat +=1
-            for item in ranked_terms.items():
-                potentials.extend([x[1] for x in item[1][:numcat]])
-                
-            potentials = list(set(potentials))
-            if numcat > 10: break
+#        while len(potentials)<=3:
+#            numcat +=1
+        for item in ranked_terms.items():
+            potentials.extend([x[1] for x in item[1][:numcat]])
+            
+        potentials = list(set(potentials))
+#            if numcat > 10: break
         
         return potentials
     
