e15705188a214b69b2fbab4b1b3d9a84f5019ee3
henne (live4adrenalin <henne (live4adrenalin@gmail.com)>
2011-04-01 13:22:07 +0200

    running stats

--- PreliminaryTests.py
+++ PreliminaryTests.py
@@ -103,11 +103,10 @@ class tester():
         
         average=[]
         for patres in patreses:
-            data = self.db.c.execute("select G.data, D.disease_name from googled_info_cleansed G, query Q, disease_info D where G.query=Q.query and Q.patres=D.patres and D.patres=?",[patres[0]]).fetchall()[0]
+            data = self.db.c.execute("select G.data, D.disease_name from googled_info_cleansed G, query Q, disease_info D where G.query=Q.query and Q.patres=D.patres and D.patres=?",[patres[0]]).fetchall()
             if data:
-                print len(data[0])
+                print len(data)
                 average.append(len(data[0]))
-                if len(data[0])>2: print "yes"
         print numpy.mean(average),"websites contained accepted information"
 
 
